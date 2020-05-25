5ae22d92d127f6d5151374fb07ce646f152f7c33
Ravindra Gadiparthi <Ravindra.Gadiparthi@vcontractor.co.za>
2020-05-25 13:10:41 +0530

    removing messages

--- cloud-cafe-course/cloudcafe-app-chapter3/src/test/java/org/cloudcafe/aws/chapter3/SimpleFileUploaderApplicationTests.java
+++ cloud-cafe-course/cloudcafe-app-chapter3/src/test/java/org/cloudcafe/aws/chapter3/SimpleFileUploaderApplicationTests.java
@@ -1,9 +1,8 @@
 package org.cloudcafe.aws.chapter3;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SimpleFileUploaderApplicationTests {
 
     @Test
